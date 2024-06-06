1d926fd6b9ffc19f4b0c06d0053a1fa11ddb43c7
Sayan Snigdha Pal <sayanpalb@gmail.com>
2024-06-06 20:49:22 +0530

    fix animation

--- client/src/components/NewsCard.jsx
+++ client/src/components/NewsCard.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 function NewsCard({ title, url }) {
   return (
-    <motion.div whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }}>
+    <motion.div whileHover={{ scale: 1.2 }}>
       <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a target="__blank" href={url}>
           <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
